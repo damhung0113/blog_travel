1d3374e6ca4450d8b94f4b1da25a76b8b76836da
TaKhacTung <k.tung130697@gmail.com>
2019-08-07 09:39:41 +0700

    Login Basic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    
     @user = User.new user_params
+
     if @user.save
       flash.now[:success] = t("controllers.user.welcome")
       redirect_to @user
@@ -24,5 +24,13 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit :name, :email, :password, :password_confirmation
+    end
+
+    def logged_in_user
+      
+      return if logged_in?
+      store_location
+      flash[:danger] = t("controllers.user.please_ln")
+      redirect_to login_url
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,35 @@ class User < ApplicationRecord
   uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: Settings.minPassword}, allow_nil: true
   has_secure_password
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update remember_digest: nil
+  end
+
+  private
+  
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest activation_token
+  end
 end
